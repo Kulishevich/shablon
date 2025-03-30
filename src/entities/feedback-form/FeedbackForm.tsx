117f997452540a9df1531a82a579e05b9bd4a0e9
Maksim Kulishevich <maxim8992012@gmail.com>
2025-03-30 17:00:37 +0300

    feat: add eslint and prettier

--- src/entities/feedback-form/FeedbackForm.tsx
+++ src/entities/feedback-form/FeedbackForm.tsx
@@ -1,32 +1,43 @@
-import React from 'react'
-import s from './FeedbackForm.module.scss'
-import Image from 'next/image'
-import { Typography } from '@/shared/ui/typography'
-import { TextField } from '@/shared/ui/text-field'
-import { TextArea } from '@/shared/ui/text-area'
-import { Checkbox } from '@/shared/ui/checkbox'
-import { Button } from '@/shared/ui/button'
+import React from 'react';
+import s from './FeedbackForm.module.scss';
+import Image from 'next/image';
+import { Typography } from '@/shared/ui/typography';
+import { TextField } from '@/shared/ui/text-field';
+import { TextArea } from '@/shared/ui/text-area';
+import { Checkbox } from '@/shared/ui/checkbox';
+import { Button } from '@/shared/ui/button';
 
 export const FeedbackForm = () => {
   return (
     <div className={s.container}>
-        <div className={s.imageContainer}>
-            <div className={s.textContent}>
-                <Typography variant='h2'>Связаться с нами</Typography>
-                <Typography variant='body_2'>Мы всегда готовы помочь вам с любыми вопросами. Свяжитесь с нами по телефону или заполните форму обратной связи.</Typography>
-            </div>
-            <Image src={'/feedback.png'} width={612} height={355} alt="feedback"/>
+      <div className={s.imageContainer}>
+        <div className={s.textContent}>
+          <Typography variant="h2">Связаться с нами</Typography>
+          <Typography variant="body_2">
+            Мы всегда готовы помочь вам с любыми вопросами. Свяжитесь с нами по
+            телефону или заполните форму обратной связи.
+          </Typography>
         </div>
+        <Image src={'/feedback.png'} width={612} height={355} alt="feedback" />
+      </div>
 
-        <div className={s.formContainer}>
-            <div className={s.form}>
-                <TextField placeholder="Введите ваше имя" label='Ваше имя' isRequired/>
-                <TextField placeholder="Введите ваш телефон" label='Ваш телефон' isRequired/>
-                <TextArea placeholder="Комментарий" label='Комментарий'/>
-                <Checkbox label="Согласие на обработку персональных данных"/>
-            </div>
-            <Button>Отправить</Button>
+      <div className={s.formContainer}>
+        <div className={s.form}>
+          <TextField
+            placeholder="Введите ваше имя"
+            label="Ваше имя"
+            isRequired
+          />
+          <TextField
+            placeholder="Введите ваш телефон"
+            label="Ваш телефон"
+            isRequired
+          />
+          <TextArea placeholder="Комментарий" label="Комментарий" />
+          <Checkbox label="Согласие на обработку персональных данных" />
         </div>
+        <Button>Отправить</Button>
+      </div>
     </div>
-  )
-}
+  );
+};
--- src/entities/news-card/NewsCard.tsx
+++ src/entities/news-card/NewsCard.tsx
@@ -1,27 +1,29 @@
-import React from 'react'
-import s from './NewsCard.module.scss'
-import Image from 'next/image'
-import { Typography } from '@/shared/ui/typography'
-import { Button } from '@/shared/ui/button'
-import { ArrowRightUpIcon } from '@/shared/assets'
-import Link from 'next/link'
+import React from 'react';
+import s from './NewsCard.module.scss';
+import Image from 'next/image';
+import { Typography } from '@/shared/ui/typography';
+import { Button } from '@/shared/ui/button';
+import { ArrowRightUpIcon } from '@/shared/assets';
+import Link from 'next/link';
 
 const news = {
-    title: 'Фурнитура для мебели: как выбрать?',
-    date: '12.02.2025',
-    image_path: "/news.png",
-}
+  title: 'Фурнитура для мебели: как выбрать?',
+  date: '12.02.2025',
+  image_path: '/news.png',
+};
 
 export const NewsCard = () => {
   return (
     <div className={s.cotnainer}>
-        <Image src={news.image_path} width={306} height={220} alt='news'/>
-        <Typography variant='body_4' className={s.date}>{news.date}</Typography>
-        <Typography variant='h5'>{news.title}</Typography>
-        <Button variant="link" as={Link} href='/'>
-            Подробнее
-            <ArrowRightUpIcon />
-        </Button>
+      <Image src={news.image_path} width={306} height={220} alt="news" />
+      <Typography variant="body_4" className={s.date}>
+        {news.date}
+      </Typography>
+      <Typography variant="h5">{news.title}</Typography>
+      <Button variant="link" as={Link} href="/">
+        Подробнее
+        <ArrowRightUpIcon />
+      </Button>
     </div>
-  )
-}
+  );
+};
