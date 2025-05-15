03a22797b4fdb20893e4b1d6828c612f7dd39d59
Dmitry Shevtsov <dshevtsov20@gmail.com>
2025-05-15 15:23:18 +0300

    feat: add gallery section to about-us page

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,17 +1,30 @@
 import { Feedback } from '@/entities/feedback/Feedback';
 import { SeoBlock } from '@/entities/seo-block';
 import { getAdvantages } from '@/shared/api/advantages/getAdvantages';
+import { getBrands } from '@/shared/api/brands/getBrands';
 import { AboutSection } from '@/widgets/about-section';
 import { AdvantagesSection } from '@/widgets/advantages-section';
+import { GallerySection } from '@/widgets/gallery-section/GallerySection';
 import { MissionSection } from '@/widgets/mission-section';
 export default async function AboutUs() {
   const advantages = await getAdvantages();
+  const brands = await getBrands();
 
   return (
     <main>
       <AboutSection />
       <MissionSection />
       <AdvantagesSection advantages={advantages} />
+      {brands && (
+        <GallerySection
+          items={
+            brands.map((brand) => ({
+              image_path: brand.image_path || '',
+              name: brand.name,
+            })) || []
+          }
+        />
+      )}
       <SeoBlock page="about-us" />
       <Feedback />
     </main>
---
+++ src/widgets/gallery-section/GallerySection.tsx
@@ -0,0 +1,80 @@
+'use client';
+import { useState, useRef } from 'react';
+import s from './GallerySection.module.scss';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import type { Swiper as SwiperType } from 'swiper';
+import Image from 'next/image';
+import { Button } from '@/shared/ui/button';
+import { ArrowLeftIcon, ArrowRightIcon } from '@/shared/assets';
+import 'swiper/css';
+import clsx from 'clsx';
+export const GallerySection = ({ items }: { items: { image_path: string; name: string }[] }) => {
+  const [activeSlide, setActiveSlide] = useState<number | undefined>(undefined);
+  const swiperRef = useRef<SwiperType>(null);
+
+  const handlePrevSlide = () => {
+    swiperRef.current?.slidePrev();
+  };
+
+  const handleNextSlide = () => {
+    swiperRef.current?.slideNext();
+  };
+
+  return (
+    <div className={s.container}>
+      <h2 className="h2">Фотогалерея</h2>
+      <div className={s.innerContainer}>
+        <Swiper
+          slidesPerView={'auto'}
+          spaceBetween={16}
+          breakpoints={{
+            768: {
+              spaceBetween: 24,
+            },
+          }}
+          onBeforeInit={(swiper) => {
+            swiperRef.current = swiper;
+          }}
+          className={s.swiper}
+        >
+          {items?.map((item, index) => (
+            <SwiperSlide key={index} className={s.swiperSlide}>
+              <Image
+                src={`${process.env.NEXT_PUBLIC_STORE_URL}/${item.image_path}`}
+                alt={item.name}
+                width={416}
+                height={340}
+                onClick={() => setActiveSlide(index)}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <div className={s.navigation}>
+          <Button variant="icon_secondary" className={s.iconLeft} onClick={handlePrevSlide}>
+            <ArrowLeftIcon />
+          </Button>
+          <Button variant="icon_secondary" className={s.iconRight} onClick={handleNextSlide}>
+            <ArrowRightIcon />
+          </Button>
+        </div>
+      </div>
+      <div
+        className={clsx(s.fullSize, { [s.active]: activeSlide !== undefined })}
+        onClick={() => setActiveSlide(undefined)}
+      >
+        {activeSlide !== undefined && (
+          <div onClick={(e) => e.stopPropagation()}>
+            <Image
+              src={`${process.env.NEXT_PUBLIC_STORE_URL}/${items?.[activeSlide].image_path}`}
+              alt={items?.[activeSlide].name || ''}
+              width={1000}
+              height={1000}
+              style={{ objectFit: 'contain' }}
+              unoptimized
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
